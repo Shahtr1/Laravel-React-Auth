e0113a566beb254e191935a69d8cc75430ee6059
Shahrukh <35841681+Shahtr1@users.noreply.github.com>
2023-01-07 05:50:01 +0530

    Completed login, signup and logout

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,50 @@
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+import { useEffect } from "react";
+import axiosClient from "../axios.client.js";
+
+export default function DefaultLayout() {
+    const { user, token, setUser, setToken } = useStateContext();
+
+    if (!token) {
+        return <Navigate to={"/login"} />;
+    }
+
+    const onLogout = (ev) => {
+        ev.preventDefault();
+
+        axiosClient.post("/logout").then(() => {
+            setUser({});
+            setToken(null);
+        });
+    };
+
+    useEffect(() => {
+        axiosClient.get("/user").then(({ data }) => {
+            setUser(data);
+        });
+    }, []);
+
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>Header</div>
+                    <div>
+                        {user.name}{" "}
+                        <a className="btn-logout" href="#" onClick={onLogout}>
+                            Logout
+                        </a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
+}
